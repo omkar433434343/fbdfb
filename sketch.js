9b841852b7ef037e587d70d799c1f04737f7619b
omkar <78024859+omkar433434343@users.noreply.github.com>
2021-02-16 17:28:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,70 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1,
+    box2,
+    box3,
+    box4;
+
+var pig1,
+    pig2;
+
+var log1,
+    log2;
+    
+var bird1;
+
+var ground;
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1 = new Box(700,320,70,70);
+
+    box2 = new Box(920,320,70,70);
+
+    box3 = new Box(700,240,70,70);
+
+    box4 = new Box(920,240,70,70);
+
+    pig1 = new Pig(200,200);
+
+    pig2 = new Pig(810,220);
+
+    log1 = new Log(810,260,300,PI/2);
+
+    log2 = new Log(810,180,300,PI/2);
+
+    bird1 = new Bird(100,100);
+
+    ground = new Ground(200,390,400,20);
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+
+    box1.display();
+   
+    box2.display();
+
+    box3.display();
+
+    box4.display();
+
+    ground.display();
+
+    pig1.display();
+
+    pig2.display();
+
+    log1.display();
+
+    log2.display();
+
+    bird1.display();
+}
